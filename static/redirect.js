c771e638138435e9828ea3dff9e6d86e761f7949
Adrian Victor <adrianvictor@disroot.org>
2026-07-02 17:09:58 -0300

    fix redirect.js

--- static/redirect.js
+++ static/redirect.js
@@ -2,6 +2,7 @@ let paramString = window.location.href.split('?')[1];
 let queryString = new URLSearchParams(paramString);
 let current = queryString.get("current");
 let action = queryString.get("action");
+let main = document.querySelector("main");
 
 let addresses = [];
 
@@ -11,28 +12,38 @@ ringMembers.forEach(member => {
 
 let currentIndex = addresses.indexOf(current);
 
-switch (action) {
-    case "next":
-        let nIndex = currentIndex - 1;
-        if (nIndex > addresses.length -1) {
+if (currentIndex === -1) {
+    main.innerHTML = `
+            <p style="color: red">Not in the webring yet!</p>
+            <p>Debug data (send this to the website maintainer) -> action = ${action}, current = ${current}</p>
+            `;
+} else {
+    switch (action) {
+    case "next": {
+        let nIndex = currentIndex + 1;
+        if (nIndex >= addresses.length) {
             nIndex = 0;
         }
+
         window.location.href = addresses[nIndex];
         break;
-    
-    case "previous":
+    }
+
+    case "previous": {
         let pIndex = currentIndex - 1;
         if (pIndex < 0) {
             pIndex = addresses.length - 1;
         }
 
         window.location.href = addresses[pIndex];
+        break;
+    }
         
     default:
-        let main = document.querySelector("main");
         main.innerHTML = `
             <p style="color: red">Error while redirecting: invalid query</p>
             <p>Debug data (send this to the website maintainer) -> action = ${action}, current = ${current}</p>
             `;
         break;
+}
 }
